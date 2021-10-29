f08d8495f5807ac4633234f6cfb70ed0df2bcdf9
Adriangw <adrianwilliamsg2684@gmail.com>
2021-10-29 13:37:46 -0300

    pruebas

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -5,7 +5,7 @@ import gRsierpinski from './Gramatica';
 const Canvas = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLCanvasElement> & React.CanvasHTMLAttributes<HTMLCanvasElement>) => {
 
   const canvasRef = useRef(null);
-  
+  /*
   const draw = (ctx: CanvasRenderingContext2D) => {
 
     ctx.beginPath()
@@ -24,20 +24,76 @@ const Canvas = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLCanva
     ctx.rotate(60 * Math.PI / 180);
     ctx.lineTo(initx+200,inity);
     ctx.stroke();
+
+
+  }
+  */
+
+  const drawSegmento = (ctx:CanvasRenderingContext2D,segmento:number=100)=>{
+
+    ctx.beginPath()
+    
+    var initx:number=0;
+    var inity:number=0;
+
+    ctx.translate(initx+100, inity+500);
+    ctx.lineTo(initx,inity);
+    ctx.rotate(300 * Math.PI / 180);
+    ctx.lineTo(initx+200,inity);   
+    ctx.translate(initx+200, inity);
+    ctx.rotate(60 * Math.PI / 180);
+    ctx.lineTo(initx+200,inity);   
+    ctx.translate(initx+200, inity);
+    ctx.rotate(60 * Math.PI / 180);
+    ctx.lineTo(initx+200,inity);
+    ctx.stroke();
+
+    
+
+  }
+
+  const rotarI = (ctx:CanvasRenderingContext2D, angulo:number):string=>{
+    return(angulo.toString());
+  }
+
+  const rotarD = (ctx:CanvasRenderingContext2D, angulo:number):string=>{
+    return(angulo.toString());
+  }
+
+  interface Map {
+    [key: string]:any;
   }
 
   useEffect(() => {
 
-    var produccion:string = gRsierpinski(5);
-    
-    
+    var produccion:string = gRsierpinski(0);
 
     var canvas:HTMLCanvasElement = canvasRef.current!;
     canvas.width=800;
     canvas.height=800;
     const ctx = canvas.getContext("2d")!;    
 
-    draw(ctx);
+    //draw(ctx);
+    //ctx.beginPath();
+
+    
+    const patron: Map = {
+      A  :drawSegmento(ctx,100),
+      B  :drawSegmento(ctx,100),
+      '+':rotarI(ctx,-60),//izquierda
+      '-':rotarD(ctx,60)//derecha
+    }
+    
+    //for (var i=0; i<produccion.length; i++) {
+      //var simbolo:string = produccion.charAt(i);
+      console.log(patron['A'])
+      //drawSegmento(ctx,100)
+ 
+    //}
+
+
+
+
 
   }, [])
   
--- src/Gramatica.tsx
+++ src/Gramatica.tsx
@@ -6,16 +6,13 @@ const gRsierpinski = (iter:number):string => {
     var B:string = 'A+B+A';
     var p1:string = A;
     var p2:string ='';
-    var p3:string ='';
-    var result:string ='';
+    var result:string ='B-A-B';
 
-    for (var i = 0; i < iter; i++) {
-        if(i>0)p1=result;
+    for (var i=0; i<iter; i++) {
+        p1=result;
         p1=p1.replaceAll('A',`A${i}`);
-        p2= B.replaceAll('A',`A${(i+1)}`);
-        p3=p1.replaceAll('B',p2);
-        result=p3.replaceAll(`A${i}`,A);
-        result=result.replaceAll(`A${(i+1)}`,'A');
+        p2=p1.replaceAll('B',B);
+        result=p2.replaceAll(`A${i}`,A);
     }  
   
     return result;
