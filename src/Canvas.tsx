c3ac0848170ce67316ab65b1f0c279f9fee45391
Adriangw <adrianwilliamsg2684@gmail.com>
2021-11-01 09:52:22 -0300

    1 de octubre

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -2,6 +2,37 @@ import React, { useRef, useEffect } from 'react';
 import { createTextSpanFromBounds } from 'typescript';
 import gRsierpinski from './Gramatica';
 
+
+
+const drawSegmento = (ctx:CanvasRenderingContext2D,segmento:number=100):string=>{
+  var initx:number=0;
+  var inity:number=0;
+
+  ctx.translate(initx+100, inity+500);
+  ctx.lineTo(initx,inity);
+  ctx.rotate(300 * Math.PI / 180);
+  ctx.lineTo(initx+200,inity);      
+  
+  ctx.stroke();
+
+  return('1')
+
+}
+
+const rotarI = (ctx:CanvasRenderingContext2D, angulo:number):string=>{
+  return(angulo.toString());
+}
+
+const rotarD = (ctx:CanvasRenderingContext2D, angulo:number):string=>{
+  return(angulo.toString());
+}
+
+interface simbolosI{
+  [index: string]:()=>string
+}
+
+
+
 const Canvas = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLCanvasElement> & React.CanvasHTMLAttributes<HTMLCanvasElement>) => {
 
   const canvasRef = useRef(null);
@@ -29,41 +60,8 @@ const Canvas = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLCanva
   }
   */
 
-  const drawSegmento = (ctx:CanvasRenderingContext2D,segmento:number=100)=>{
-
-    ctx.beginPath()
-    
-    var initx:number=0;
-    var inity:number=0;
-
-    ctx.translate(initx+100, inity+500);
-    ctx.lineTo(initx,inity);
-    ctx.rotate(300 * Math.PI / 180);
-    ctx.lineTo(initx+200,inity);   
-    ctx.translate(initx+200, inity);
-    ctx.rotate(60 * Math.PI / 180);
-    ctx.lineTo(initx+200,inity);   
-    ctx.translate(initx+200, inity);
-    ctx.rotate(60 * Math.PI / 180);
-    ctx.lineTo(initx+200,inity);
-    ctx.stroke();
-
-    
-
-  }
-
-  const rotarI = (ctx:CanvasRenderingContext2D, angulo:number):string=>{
-    return(angulo.toString());
-  }
-
-  const rotarD = (ctx:CanvasRenderingContext2D, angulo:number):string=>{
-    return(angulo.toString());
-  }
-
-  interface Map {
-    [key: string]:any;
-  }
-
+  
+  
   useEffect(() => {
 
     var produccion:string = gRsierpinski(0);
@@ -74,26 +72,19 @@ const Canvas = (props: JSX.IntrinsicAttributes & React.ClassAttributes<HTMLCanva
     const ctx = canvas.getContext("2d")!;    
 
     //draw(ctx);
-    //ctx.beginPath();
-
     
-    const patron: Map = {
-      A  :drawSegmento(ctx,100),
-      B  :drawSegmento(ctx,100),
-      '+':rotarI(ctx,-60),//izquierda
-      '-':rotarD(ctx,60)//derecha
+
+    const simbolos:simbolosI = {
+      'A' :()=>drawSegmento(ctx,100),
+      'B' :()=>drawSegmento(ctx,100),
+      '+':()=>rotarI(ctx,-60),//izquierda
+      '-':()=>rotarD(ctx,60)//derecha
     }
-    
+  
     //for (var i=0; i<produccion.length; i++) {
-      //var simbolo:string = produccion.charAt(i);
-      console.log(patron['A'])
-      //drawSegmento(ctx,100)
- 
+      var simbolo:string = produccion.charAt(0);
+      simbolos[simbolo]();
     //}
-
-
-
-
 
   }, [])
   
